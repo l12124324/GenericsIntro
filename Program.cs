5f8bc6e921d160bb015b9f2d8187576444f7dac3
Mert3322 <77886004+l12124324@users.noreply.github.com>
2021-02-06 13:25:54 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MYList<string> isimler = new MYList<string>();
+            isimler.Add("Engin");
+
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Kerem");
+            Console.WriteLine(isimler.Length);
+
+
+
+        }
+    }
+}
